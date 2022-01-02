dc9be0b942ea2852035f8d65e8cbe75c7921b57a
Aditya <aditya20_ug@cse.nits.ac.in>
2022-01-02 23:29:00 +0530

    with active on navbar on gallery page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // const banner=document.querySelector(".Banner_start");
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector("ul");
+const links =navMenu.getElementsByClassName("links");
 const image=document.querySelector(".image");
 const navbar=document.querySelector(".navbar");
 const body=document.body;
@@ -54,3 +55,20 @@ function closeMenu() {
     navbar.classList.toggle("blur");
 }
 console.log(navMenu)
+
+
+
+
+for (var i = 0; i < links.length; i++) {
+    if(i==3){
+        links[3].style.hover="none";
+        continue;
+    }
+    links[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("underline");
+    current[0].className = current[0].className.replace("underline", "");
+    this.className += " underline";
+    // this.style.borderBottom="1px solid black"
+    // this.parentElement.classList.toggle("underline");
+    });
+  }
